bdb85bd8f832fabb317ee48a8a9951c5e2ab464d
aziz muhammad naufal <ajosnaufal@gmail.com>
2022-09-30 09:24:01 +0700

    add image loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import generateJoke from "./generateJoke";
 import './styles/main.scss'
+import laughing from './assets/laughing.svg'
 
-console.log(generateJoke())
+const laughImg = document.getElementById('laughing')
+const jokeContainer = document.getElementById('the-joke')
+laughImg.src = laughing
+
+
+jokeContainer.innerText = generateJoke()
+
 console.log('1')
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name][contenthash].js",
     clean: true,
+    assetModuleFilename: '[name][ext]',
   },
+  devtool: 'source-map',
   devServer: {
     static: {
         directory: path.resolve(__dirname, 'dist')
@@ -24,6 +26,20 @@ module.exports = {
   module: {
     rules: [
       { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
     ],
   },
   plugins: [
